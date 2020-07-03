4836c557bf7900d382484397e2d7f6090e4e2e41
Rafa <rafaelgdotta@gmail.com>
2020-07-03 00:28:46 -0300

    Add redux persistent state to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import { loadState, saveState } from "./localstorage";
 
-export default createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(() => {
+  console.log(store.getState());
+  saveState({
+    todos: store.getState().todos,
+  });
+});
+
+export default store;
