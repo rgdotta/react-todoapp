223d7b3baddabf8d40934003db7cb272df6a4f0c
Rafa <rafaelgdotta@gmail.com>
2020-07-24 14:05:47 -0300

    Removed residual code from last commit

--- src/components/Lists/Subtask.js
+++ src/components/Lists/Subtask.js
@@ -1,44 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { Checkbox } from "@material-ui/core";
 import deleteIcon from "../../css/images/icone_deletar_tarefa-subtarefa.png";
 
 const Subtask = (props) => {
-  const [isComplete, setComplete] = useState(false);
-
-  function completeTask() {
-    setComplete(!isComplete);
-
-    if (isComplete === true) {
-      props.complete(-1);
-    } else {
-      props.complete(1);
-    }
-  }
-
-  useEffect(() => {
-    if (props.taskCompleted) {
-      setComplete(true);
-    }
-  }, [props.taskCompleted]);
-
   return (
     <li className="subTask flex row">
       <Checkbox
         className="taskCheckbox"
-        onClick={completeTask}
-        checked={isComplete}
+        onClick={() => props.toggleComplete(props.listId, props.subtask.id)}
+        checked={props.subtask.complete === true}
       />
       <p
         className="taskBody"
-        style={{ textDecoration: isComplete ? "line-through" : "none" }}
+        style={{
+          textDecoration: props.subtask.complete ? "line-through" : "none",
+        }}
       >
-        {props.subtask}
+        {props.subtask.name}
       </p>
       {props.editable && (
         <button
           className="taskDeleteBtn noStyleBtn"
-          onClick={() => props.delete(props.id)}
+          onClick={() => props.delete(props.subtask.id)}
         >
           <img src={deleteIcon} alt="delete" />
         </button>
